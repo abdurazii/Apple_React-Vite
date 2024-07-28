1d0acc9fc983fb88f0fb5206c88b987c0e28eb3e
Abdul Razick <abdulrazick007@gmail.com>
2024-07-28 12:31:02 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import HowItWorks from "./components/HowItWorks"
+import Model from "./components/Model"
+import NavBar from "./components/NavBar"
+
+
+const  App = () => {
+
+
+  return (
+    <main className="bg-black">
+      <NavBar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
